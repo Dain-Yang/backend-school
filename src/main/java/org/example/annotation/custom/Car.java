5aa415481e0653c9acf06d8e4c755bac5595f0f1
Dain-Yang <yang.dain.24@gmail.com>
2025-06-24 23:35:32 +0900

    [Annotation, Reflection] Custom Annotation: Method Type

--- src/main/java/org/example/annotation/custom/Car.java
+++ src/main/java/org/example/annotation/custom/Car.java
@@ -1,5 +1,8 @@
 package org.example.annotation.custom;
 
+import org.example.annotation.custom.annotation.ModelDescriptionPrint;
+import org.example.annotation.custom.annotation.ModelDescriptionPrints;
+
 public class Car {
     private final String model;
     // 1980 ~ 2025
@@ -10,8 +13,45 @@ public class Car {
         this.year = year;
     }
 
+@ModelDescriptionPrints({
+        @ModelDescriptionPrint(model = "Sonata", description = "steady seller"),
+        @ModelDescriptionPrint(model = "Ionic", description = "new model"),
+        @ModelDescriptionPrint(model = "Grandeur", description = "luxury model"),
+        @ModelDescriptionPrint(model = "Avante", description = "compact model"),
+        @ModelDescriptionPrint(model = "Morning", description = "small car"),
+})
     public String getModel() {
-        return model;
+        //switch (model) {
+        //    case "Ionic":
+        //        System.out.println("new model");
+        //        break;
+        //    case "Sonata":
+        //        System.out.println("steady seller");
+        //        break;
+        //    case "Grandeur":
+        //        System.out.println("luxury model");
+        //        break;
+        //    case "Avante":
+        //        System.out.println("compact model");
+        //        break;
+        //    case "Morning":
+        //        System.out.println("small car");
+        //        break;
+        //    default:
+        //        System.out.println("unknown model");
+        //}
+    try {
+        ModelDescriptionPrint[] annotations = this.getClass().getMethod("getModel").getAnnotationsByType(ModelDescriptionPrint.class);
+        for (ModelDescriptionPrint annotation : annotations) {
+            if (annotation.model().equals(model)) {
+                System.out.println(annotation.description());
+            }
+        }
+    } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+    }
+
+    return model;
     }
 
     public int getYear() {
