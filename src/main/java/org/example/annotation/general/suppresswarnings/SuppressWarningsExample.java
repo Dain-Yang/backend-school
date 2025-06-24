f52ba5225c6a55e646d4d6268f2d30650009f25b
Dain-Yang <yang.dain.24@gmail.com>
2025-06-24 20:44:53 +0900

    [Annotation, Reflection] General Annotation: SuppressWarnings

---
+++ src/main/java/org/example/annotation/general/suppresswarnings/SuppressWarningsExample.java
@@ -0,0 +1,12 @@
+package org.example.annotation.general.suppresswarnings;
+
+import java.util.ArrayList;
+
+public class SuppressWarningsExample {
+    @SuppressWarnings({"unchecked", "rawtypes", "unused"}) //잘 쓰이지는 않지만 레거시 코드를 처리할 때 가끔 사용
+    public void something() {
+        List list = new ArrayList();
+        list.add("data");
+        System.out.println(list.get(0));
+    }
+}
